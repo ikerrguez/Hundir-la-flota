cba82a0c1b935c455ff82bd8e851d9641eb9eaf1
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-12-09 20:27:22 +0100

    Utilidades casi terminado

--- src/Utilidades.java
+++ src/Utilidades.java
@@ -8,8 +8,7 @@ public class Utilidades {
      * Devuelve un número entero aleatorio entre min y max (ambos incluidos).
      */
     public static int numeroAleatorio(int min, int max) {
-        // TODO
-        return -1;
+        return (int)(Math.random() * (max - min + 1)) + min; //devuelve un número aleatorio entre min y máx (ambos incluids)
     }
 
     /**
@@ -20,10 +19,10 @@ public class Utilidades {
      *
      * Si la coordenada no es válida, debe devolver -1.
      */
-    public static int convertirFila(String coord) {
-        // TODO: extraer la parte numérica y convertirla a int
-
-        return -1;
+    public static int convertirFila(String coord) { //convierte la coordenada en fila
+        if (coord.length() < 2) return -1; //si es muy corta, no es válida
+        String numero = coord.substring(1); //quita le letra y guarda sólo el número
+        return Integer.parseInt(numero); //convierte el texto a número
     }
 
     /**
@@ -33,7 +32,7 @@ public class Utilidades {
      * Si la coordenada no es válida, puede devolver -1.
      */
     public static int convertirColumna(String coord) {
-        // TODO
+       // TODO
         return -1;
     }
 
